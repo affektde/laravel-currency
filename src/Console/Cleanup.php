7ef8c941b81786bc8bf2daf1703173cb40ff4ba4
Benjamin Takats <mail@benjamintakats.de>
2017-10-04 15:11:47 +0200

    Update

--- src/Console/Cleanup.php
+++ src/Console/Cleanup.php
@@ -3,53 +3,55 @@
 namespace Torann\Currency\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 
 class Cleanup extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'currency:cleanup';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'currency:cleanup';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Cleanup currency cache';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Cleanup currency cache';
 
-    /**
-     * Currency instance
-     *
-     * @var \Torann\Currency\Currency
-     */
-    protected $currency;
+  /**
+   * Currency instance
+   *
+   * @var \Torann\Currency\Currency
+   */
+  protected $currency;
 
-    /**
-     * Create a new command instance.
-     */
-    public function __construct()
-    {
-        $this->currency = app('currency');
+  /**
+   * Create a new command instance.
+   */
+  public function __construct()
+  {
+    $this->currency = app('currency');
 
-        parent::__construct();
-    }
+    parent::__construct();
+  }
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
-    public function fire()
-    {
-        // Clear cache
-        $this->currency->clearCache();
-        $this->comment('Currency cache cleaned.');
+  /**
+   * Execute the console command.
+   *
+   * @return void
+   */
+  public function handle()
+  {
+    Config::set('database.default', 'public');
+    // Clear cache
+    $this->currency->clearCache();
+    $this->comment('Currency cache cleaned.');
 
-        // Force the system to rebuild cache
-        $this->currency->getCurrencies();
-        $this->comment('Currency cache rebuilt.');
-    }
+    // Force the system to rebuild cache
+    $this->currency->getCurrencies();
+    $this->comment('Currency cache rebuilt.');
+  }
 }
