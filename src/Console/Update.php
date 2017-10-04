2fbb40d18809f1e3e27e9f4eeb782dbc9dea17ac
Benjamin Takats <mail@benjamintakats.de>
2017-10-04 14:22:55 +0200

    Update

--- src/Console/Update.php
+++ src/Console/Update.php
@@ -7,146 +7,153 @@ use Illuminate\Console\Command;
 
 class Update extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'currency:update
+  use \Labelcontrol\Traits\DatabaseTrait;
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'currency:update
                                 {--o|openexchangerates : Get rates from OpenExchangeRates.org}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Update exchange rates from an online source';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Update exchange rates from an online source';
 
-    /**
-     * Currency instance
-     *
-     * @var \Torann\Currency\Currency
-     */
-    protected $currency;
+  /**
+   * Currency instance
+   *
+   * @var \Torann\Currency\Currency
+   */
+  protected $currency;
 
-    /**
-     * Create a new command instance.
-     */
-    public function __construct()
-    {
-        $this->currency = app('currency');
+  /**
+   * Create a new command instance.
+   */
+  public function __construct()
+  {
+    $this->currency = app('currency');
 
-        parent::__construct();
+    parent::__construct();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return void
+   */
+  public function handle()
+  {
+    $this->switchSchema('manager');
+    $apps = \Labelcontrol\Models\Application::where('hash', 'raejb')->get();
+    foreach ($apps as $app) {
+      $this->switchSchema($app->hash);
+      $this->info($app->hash);
+
+      // Get Settings
+      $defaultCurrency = $this->currency->config('default');
+
+      if ($this->input->getOption('openexchangerates')) {
+        if (!$api = $this->currency->config('api_key')) {
+          $this->error('An API key is needed from OpenExchangeRates.org to continue.');
+
+          return;
+        }
+
+        // Get rates
+        $this->updateFromOpenExchangeRates($defaultCurrency, $api);
+      } else {
+        // Get rates
+        $this->updateFromYahoo($defaultCurrency);
+      }
+    }
+  }
+
+  private function updateFromYahoo($defaultCurrency)
+  {
+    $this->comment('Updating currency exchange rates from Finance Yahoo...');
+
+    $data = [];
+
+    // Get all currencies
+    foreach ($this->currency->getDriver()->all() as $code => $value) {
+      $data[] = "{$defaultCurrency}{$code}=X";
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
-    public function fire()
-    {
-        // Get Settings
-        $defaultCurrency = $this->currency->config('default');
+    // Ask Yahoo for exchange rate
+    if ($data) {
+      $content = $this->request('http://download.finance.yahoo.com/d/quotes.csv?s=' . implode(',', $data) . '&f=sl1&e=.csv');
 
-        if ($this->input->getOption('openexchangerates')) {
-            if (!$api = $this->currency->config('api_key')) {
-                $this->error('An API key is needed from OpenExchangeRates.org to continue.');
+      $lines = explode("\n", trim($content));
 
-                return;
-            }
+      // Update each rate
+      foreach ($lines as $line) {
+        $code = substr($line, 4, 3);
+        $value = substr($line, 11, 6) * 1.00;
 
-            // Get rates
-            $this->updateFromOpenExchangeRates($defaultCurrency, $api);
+        if ($value) {
+          $this->currency->getDriver()->update($code, [
+            'exchange_rate' => $value,
+          ]);
         }
-        else {
-            // Get rates
-            $this->updateFromYahoo($defaultCurrency);
-        }
+      }
+
+      // Clear old cache
+      $this->call('currency:cleanup');
     }
 
-    private function updateFromYahoo($defaultCurrency)
-    {
-        $this->comment('Updating currency exchange rates from Finance Yahoo...');
+    $this->info('Complete');
+  }
 
-        $data = [];
+  private function updateFromOpenExchangeRates($defaultCurrency, $api)
+  {
+    $this->info('Updating currency exchange rates from OpenExchangeRates.org...');
 
-        // Get all currencies
-        foreach ($this->currency->getDriver()->all() as $code => $value) {
-            $data[] = "{$defaultCurrency}{$code}=X";
-        }
+    // Make request
+    $content = json_decode($this->request("http://openexchangerates.org/api/latest.json?base={$defaultCurrency}&app_id={$api}"));
 
-        // Ask Yahoo for exchange rate
-        if ($data) {
-            $content = $this->request('http://download.finance.yahoo.com/d/quotes.csv?s=' . implode(',', $data) . '&f=sl1&e=.csv');
+    // Error getting content?
+    if (isset($content->error)) {
+      $this->error($content->description);
 
-            $lines = explode("\n", trim($content));
-
-            // Update each rate
-            foreach ($lines as $line) {
-                $code = substr($line, 4, 3);
-                $value = substr($line, 11, 6) * 1.00;
-
-                if ($value) {
-                    $this->currency->getDriver()->update($code, [
-                        'exchange_rate' => $value,
-                    ]);
-                }
-            }
-
-            // Clear old cache
-            $this->call('currency:cleanup');
-        }
-
-        $this->info('Complete');
+      return;
     }
 
-    private function updateFromOpenExchangeRates($defaultCurrency, $api)
-    {
-        $this->info('Updating currency exchange rates from OpenExchangeRates.org...');
+    // Parse timestamp for DB
+    $timestamp = new DateTime(strtotime($content->timestamp));
 
-        // Make request
-        $content = json_decode($this->request("http://openexchangerates.org/api/latest.json?base={$defaultCurrency}&app_id={$api}"));
-
-        // Error getting content?
-        if (isset($content->error)) {
-            $this->error($content->description);
-
-            return;
-        }
-
-        // Parse timestamp for DB
-        $timestamp = new DateTime(strtotime($content->timestamp));
-
-        // Update each rate
-        foreach ($content->rates as $code => $value) {
-            $this->currency->getDriver()->update($code, [
-                'exchange_rate' => $value,
-                'updated_at' => $timestamp,
-            ]);
-        }
-
-        $this->currency->clearCache();
-
-        $this->info('Update!');
+    // Update each rate
+    foreach ($content->rates as $code => $value) {
+      $this->currency->getDriver()->update($code, [
+        'exchange_rate' => $value,
+        'updated_at' => $timestamp,
+      ]);
     }
 
-    private function request($url)
-    {
-        $ch = curl_init($url);
+    $this->currency->clearCache();
 
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1");
-        curl_setopt($ch, CURLOPT_HTTPGET, true);
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 2);
-        curl_setopt($ch, CURLOPT_MAXCONNECTS, 2);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $this->info('Update!');
+  }
 
-        $response = curl_exec($ch);
-        curl_close($ch);
+  private function request($url)
+  {
+    $ch = curl_init($url);
 
-        return $response;
-    }
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1");
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 2);
+    curl_setopt($ch, CURLOPT_MAXCONNECTS, 2);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+  }
 }
